cdb7df53453f0193ddff878861e72539ea5d29e5
Gustavo Barcario <gustavobarcario.0788@gmail.com>
2023-02-19 12:33:57 -0300

    Cria Metodo Alterar e Excluir

--- br.com.controlevendas.dao/ClienteDAO.cs
+++ br.com.controlevendas.dao/ClienteDAO.cs
@@ -54,6 +54,69 @@ namespace Controle_de_Vendas.br.com.controlevendas.dao
         }
         #endregion
 
+        #region Alterar Cliente
+        public void alterarCliente(Cliente obj)
+        {
+            try
+            {
+                string sql = @"update tb_clientes set (@nome=nome,@rg=rg,#cpf=cpf,@email=email,@telefone=telefone,@celular=celular,@cep=cep,@endereco=endereco,@numero=numero,@complemento=complemento,@bairro=bairro,@cidade=cidade,@estado=estado)
+                where id = @id";
+
+                SqlCommand executacmd = new SqlCommand(sql, conexao);
+                executacmd.Parameters.AddWithValue("@nome", obj.nome);
+                executacmd.Parameters.AddWithValue("@rg", obj.rg);
+                executacmd.Parameters.AddWithValue("@cpf", obj.cpf);
+                executacmd.Parameters.AddWithValue("@email", obj.email);
+                executacmd.Parameters.AddWithValue("@telefone", obj.telefone);
+                executacmd.Parameters.AddWithValue("@celular", obj.celular);
+                executacmd.Parameters.AddWithValue("@cep", obj.cep);
+                executacmd.Parameters.AddWithValue("@endereco", obj.endereco);
+                executacmd.Parameters.AddWithValue("@numero", obj.numero);
+                executacmd.Parameters.AddWithValue("@complemento", obj.complemento);
+                executacmd.Parameters.AddWithValue("@bairro", obj.bairro);
+                executacmd.Parameters.AddWithValue("@cidade", obj.cidade);
+                executacmd.Parameters.AddWithValue("@estado", obj.estado);
+
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente alterado com sucesso!");
+            }
+            catch (System.Exception erro)
+            {
+
+                MessageBox.Show("Aconteceu um erro" + erro);
+            }
+
+        }
+        #endregion
+
+        #region Excluir Cliente
+        public void exluirCliente(Cliente obj)
+        {
+            try
+            {
+                string sql = @"delete from tb_clientes where id = @id";
+
+                SqlCommand executacmd = new SqlCommand(sql, conexao);
+                executacmd.Parameters.AddWithValue("@id", obj.codigo);
+
+                conexao.Open();
+                executacmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cliente excluido com sucesso!");
+            }
+            catch (System.Exception erro)
+            {
+
+                MessageBox.Show("Aconteceu um erro" + erro);
+            }
+
+        }
+        #endregion
+
         #region Listar Clientes
         public DataTable listarClientes()
         {
@@ -79,5 +142,7 @@ namespace Controle_de_Vendas.br.com.controlevendas.dao
             }
         }
         #endregion
+
+
     }
 }
